f6277ad77360eb7d22292f034c2ad237d4a6b863
Dylan <besonend@gmail.com>
2020-08-14 16:41:23 -0400

    Got the search bar working for browse

---
+++ routes/browse-books/genres.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const { routeHandler } = require('../utils');
+const { Op } = require("sequelize");
+const { Book, Bookshelf, Author, Review, Publisher, Genre, Series } = require('../../db/models');
+
+router.get('/', routeHandler(async (req, res) => {
+    const genres = await Genre.findAll({
+        include: [{ model: Book }, { model: Series }],
+    });
+
+    res.render('genre-browse', { title: "Browse Books", genres })
+}));
+
+router.get('/:id', routeHandler(async (req, res) => {
+    const id = req.params.id;
+    const genres = await Genre.findAll({
+        where: {
+            name: {
+                [Op.substring]: id,
+            },
+        },
+        include: [{ model: Book }, { model: Series }],
+    });
+
+    res.render('genre-browse', { title: "Browse Books", genres })
+}));
+
+module.exports = router;
--- routes/browse-books/titles.js
+++ routes/browse-books/titles.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const titleRouter = require("./titles");
-const authorRouter = require('./authors');
-const genreRouter = require('./genres');
-const { environment } = require('../../config')
-const { ValidationError } = require("sequelize");
 const { routeHandler } = require('../utils');
+const { Op } = require("sequelize");
 const { Book, Bookshelf, Author, Review, Publisher, Genre, Series } = require('../../db/models');
-
-router.use("/Titles", titleRouter);
-router.use('/Authors', authorRouter);
-router.use('/Genres', genreRouter);
 
 router.get('/', routeHandler(async (req, res) => {
     const books = await Book.findAll({
@@ -18,7 +10,19 @@ router.get('/', routeHandler(async (req, res) => {
     });
 
     res.render('books-browse', { title: "Browse Books", books })
+}));
 
+router.get('/:id', routeHandler(async (req, res) => {
+    const id = req.params.id;
+    const books = await Book.findAll({
+        where: {
+            title: {
+                [Op.substring]: id,
+            },
+        },
+        include: [{ model: Publisher }, { model: Author }, { model: Genre }, { model: Review }, { model: Series }],
+    });
+    res.render('books-browse', { title: "Browse Books", books })
 }));
 
 module.exports = router;
