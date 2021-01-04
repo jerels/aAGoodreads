f12e147e4a9757f7970365a5844a120f2ac04b20
Jerel <65048179+jerels@users.noreply.github.com>
2021-01-04 17:57:50 -0600

    number of reviews shows up on the profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,10 @@
+const reviews = document.getElementById('reviews');
+const ratings = document.getElementById('ratings');
+
 async function getUser() {
     const res = await fetch('/api/users/profile');
     const data = await res.json();
+    reviews.innerHTML = `${data.numOfReviews} reviews`
     console.log(data);
 };
 
