932c18a392e7f807fb8b23e598f714d6eeb208ce
Jerel <65048179+jerels@users.noreply.github.com>
2021-01-04 18:07:19 -0600

    activity updates on profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,11 +1,13 @@
 const reviews = document.getElementById('reviews');
 const ratings = document.getElementById('ratings');
+const activity = document.querySelector('.activity-content');
 
 async function getUser() {
     const res = await fetch('/api/users/profile');
     const data = await res.json();
     reviews.innerHTML = `${data.numOfReviews} reviews`;
     ratings.innerHTML = `${data.ratingTotal} ratings (${data.reviewAvg} avg)`;
+    activity.innerHTML = `Joined in ${data.date}`;
     console.log(data);
 };
 
