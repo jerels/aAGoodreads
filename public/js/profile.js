4b0acb33a20932b8f597f9a62eb8157772c532f4
Jerel <65048179+jerels@users.noreply.github.com>
2021-01-04 18:18:42 -0600

    user shelves appear on the profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,7 @@
 const reviews = document.getElementById('reviews');
 const ratings = document.getElementById('ratings');
 const activity = document.querySelector('.activity-content');
+const bookshelves = document.querySelector('.bookshelves-container');
 
 async function getUser() {
     const res = await fetch('/api/users/profile');
@@ -8,6 +9,12 @@ async function getUser() {
     reviews.innerHTML = `${data.numOfReviews} reviews`;
     ratings.innerHTML = `${data.ratingTotal} ratings (${data.reviewAvg} avg)`;
     activity.innerHTML = `Joined in ${data.date}`;
+    for (const shelf in data.bookshelfObj) {
+        const bookshelf = document.createElement('p');
+        bookshelf.setAttribute('class', 'bookshelf');
+        bookshelf.innerHTML = `${shelf.toLowerCase()} (${data.bookshelfObj[shelf]})`;
+        bookshelves.appendChild(bookshelf);
+    };
     console.log(data);
 };
 
