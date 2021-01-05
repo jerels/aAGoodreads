1def039daf6cdcf654526c47f9791abbc1aff5a1
Jerel <65048179+jerels@users.noreply.github.com>
2021-01-04 18:01:05 -0600

    number of ratings and rating average shows up on the profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,7 +4,8 @@ const ratings = document.getElementById('ratings');
 async function getUser() {
     const res = await fetch('/api/users/profile');
     const data = await res.json();
-    reviews.innerHTML = `${data.numOfReviews} reviews`
+    reviews.innerHTML = `${data.numOfReviews} reviews`;
+    ratings.innerHTML = `${data.ratingTotal} ratings (${data.reviewAvg} avg)`;
     console.log(data);
 };
 
